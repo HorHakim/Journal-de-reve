24928326d85df8b7505e655b2defb80e89db0679
HorHakim <horairyhakim@gmail.com>
2025-02-22 00:01:22 +0100

    Add additional account fields

--- dream_tracker_backend/accounts/serializers.py
+++ dream_tracker_backend/accounts/serializers.py
@@ -4,11 +4,23 @@ from rest_framework import serializers
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True}}
+
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Le mot de passe doit contenir au moins 8 caractères.")
+        return value
+
+    def validate_email(self, value):
+        if '@' not in value:
+            raise serializers.ValidationError("L'email n'est pas valide.")
+        return value
+
+
 
     def create(self, validated_data):
         # Utilisation de la méthode create_user pour gérer le hash du mot de passe
-        print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
--- dream_tracker_backend/accounts/test.py
+++ dream_tracker_backend/accounts/test.py
@@ -6,32 +6,72 @@ from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 
 User = get_user_model()
+registration_url = reverse('accounts:register')
+login_url = reverse('accounts:login')
+user_creation_data = {
+    'username': 'testuser',
+    'email': 'labalette.antoine@gmail.com',
+    'first_name': 'Antoine',
+    'last_name': 'Labalette',
+    'password': '#TestPassword123'
+}
 
 class RegistrationTest(APITestCase):
+
     def test_registration(self):
-        url = reverse('accounts:register')
-        data = {
-            'username': 'testuser',
-            'password': 'TestPassword123'
-        }
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(registration_url, user_creation_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # Vérifier que l'utilisateur a bien été créé
-        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+        user = User.objects.filter(username='testuser')
+        self.assertTrue(user.exists())
+        user_instance = user.first()
+
+        for field_name, value in user_creation_data.items():
+            if field_name != "password":
+                self.assertEqual(getattr(user_instance, field_name), value)
+
+
+    def test_user_already_exists(self):
+        self.user = User.objects.create_user(**user_creation_data)
+        response = self.client.post(registration_url, user_creation_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+
+    def test_password_too_simple(self):
+        wrong_data = user_creation_data.copy()
+        wrong_data['password'] = "Anton"
+        response = self.client.post(registration_url, wrong_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_email_invalid(self):
+        wrong_data = user_creation_data.copy()
+        wrong_data['email'] = "Anton"
+        response = self.client.post(registration_url, wrong_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 
 class LoginTest(APITestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='TestPassword123')
+        self.user = User.objects.create_user(**user_creation_data)
     
     def test_login(self):
-        url = reverse('accounts:login')
+
+        data = {
+            'username': 'testuser',
+            'password': '#TestPassword123'
+        }
+        response = self.client.post(login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Le test attend la présence d'un token dans la réponse
+        self.assertIn('token', response.data)
+
+    def test_wrong_login(self):
+
         data = {
             'username': 'testuser',
             'password': 'TestPassword123'
         }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Le test attend la présence d'un token dans la réponse
-        self.assertIn('token', response.data)
+        response = self.client.post(login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
